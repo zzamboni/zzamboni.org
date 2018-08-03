ac8646d90cfcc152f09e661b76fe90aa0c977a85
Diego Zamboni <diego@zzamboni.org>
2018-08-03 11:43:30 +0200

    If any arguments are given, they are used as commit message, otherwise "Regen" is used

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# Regenerate with Hugo and upload
+# Arguments (if any) are used as commit message, otherwise a generic one is used
+if [ $# -gt 0 ]; then
+  MSG="$@"
+else
+  MSG="Regen"
+fi
 export HUGO_ENV=production
 hugo
 git add docs
-git ci -m 'Regen'
+git ci -m "$MSG"
 git push
